1a90b8f682bd56253f0d7139a66930ba51097d73
Yong <yong.lin13@gmail>
2023-02-27 22:28:37 -0500

    W8D2

---
+++ W8D2/homework.js
@@ -0,0 +1,7 @@
+function madLib(verb, adject, noun) {
+    return "We shall ${verb.toUpperCase()} the ${adject.toUpperCase()} ${noun.toUpperCase()}"
+}
+
+function isSubstring(phrase, subphrase) {
+  return phrase.includes(subphrase);
+}
