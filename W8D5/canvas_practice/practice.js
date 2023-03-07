0faf137a9a00aa27499d35c8fcd798cff7137fa6
Yong <yong.lin13@gmail>
2023-03-06 21:32:45 -0500

    W9D2

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    const canvasEl = document.getElementById("mycanvas");
+    canvasEl.width = 500;
+    canvasEl.height = 500;
+  
+    const ctx = canvasEl.getContext("2d");
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 0, 500, 500);
+  
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.fill();
 });
