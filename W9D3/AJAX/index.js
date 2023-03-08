73f6c43400667c7ed74385d3c9c03d7670a8e34a
Yong <yong.lin13@gmail>
2023-03-08 09:10:32 -0500

    updated W9D3 HW

--- W9D3/AJAX/index.js
+++ W9D3/AJAX/index.js
@@ -2,7 +2,21 @@ console.log("Hello from the console!");
 
 // Your code here
 fetch("https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b")
-    .then(response => response.json())
+    .then(response => {
+        if (response.ok){
+            return response.json()
+        } else {
+            throw response; 
+        }
+    })
     .then(post => console.log(post)
-
+    .catch(errorResponse => console.error(errorResponse))
     );
+
+console.log("The AJAX request has been dispatched.");
+
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b")
+//     .then(response => response.json())
+//     .then(post => console.log(post)
+//     .catch(errorResponse => console.error(errorResponse))
+// );
